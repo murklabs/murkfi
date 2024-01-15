1adcd1d0c98f6fccc8a17db3157106699a1e951c
mmnavarr <mmnavarr@live.com>
2024-01-15 16:11:57 -0600

    Tucked impl details to PDA impls and implemented attempted burn

--- programs/vault/programs/vault/src/instructions/vault.rs
+++ programs/vault/programs/vault/src/instructions/vault.rs
@@ -2,7 +2,7 @@ use crate::error::MurkError;
 use crate::state::events::*;
 use crate::state::{state::State, vault::Vault};
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Mint, MintTo, Token, TokenAccount, Transfer};
+use anchor_spl::token::{self, Mint, MintTo, Token, TokenAccount, Transfer, Burn};
 use core::mem::size_of;
 
 pub fn handle_create_vault(ctx: Context<CreateVault>) -> Result<()> {
@@ -28,21 +28,13 @@ pub fn handle_deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
     ctx.accounts.validate_deposit()?;
 
     // 1. Deposit token from user ATA to the vault ATA
-    let cpi_accounts = Transfer {
-        from: ctx.accounts.user_token_account.to_account_info(),
-        to: ctx.accounts.vault_token_account.to_account_info(),
-        authority: ctx.accounts.signer.to_account_info(),
-    };
-    let cpi_program = ctx.accounts.token_program.to_account_info();
-    let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-    token::transfer(cpi_ctx, amount)?;
+    ctx.accounts.deposit_to_vault(amount)?;
 
     // 2. Create user vToken ATA
     // TODO: Implement
 
     // 3. Mint tokens to the user's vault ATA
-    let (_, bump) = Pubkey::find_program_address(&[b"mint_authority"], ctx.program_id);
-    ctx.accounts.mint_tokens_to_user(amount, bump)?;
+    ctx.accounts.mint_vtokens_to_user(ctx.program_id, amount)?;
 
     emit!(VaultDeposit {
         amount: amount,
@@ -61,24 +53,10 @@ pub fn handle_withdrawal(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
     // TODO: Implement
 
     // 2. Transfer USDC from vault usdc ATA to user usdc ATA
-    let (_, bump) = Pubkey::find_program_address(
-        &[b"vault", ctx.accounts.vault.id.to_le_bytes().as_ref()],
-        ctx.program_id,
-    );
-    let vault_id = ctx.accounts.vault.id.to_le_bytes();
-    let seeds: &[&[u8]; 3] = &[b"vault", vault_id.as_ref(), &[bump][..]];
-    let signer = &[&seeds[..]];
-    let cpi_accounts = Transfer {
-        from: ctx.accounts.vault_token_account.to_account_info(),
-        to: ctx.accounts.withdrawal_token_account.to_account_info(),
-        authority: ctx.accounts.vault.to_account_info(),
-    };
-    let cpi_program = ctx.accounts.token_program.to_account_info();
-    let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
-    token::transfer(cpi_ctx, amount)?;
+    ctx.accounts.withdraw_from_vault(ctx.program_id, amount)?;
 
     // 3. Burn vToken from user vToken ATA
-    // TODO: Implement
+    // ctx.accounts.burn_vtoken_from_user(amount)?;
 
     emit!(VaultWithdrawal {
         amount: amount,
@@ -194,7 +172,20 @@ impl Deposit<'_> {
 
         Ok(())
     }
-    fn mint_tokens_to_user(&self, amount: u64, bump: u8) -> Result<()> {
+    fn deposit_to_vault(&self, amount: u64) -> Result<()> {
+        let cpi_accounts = Transfer {
+            from: self.user_token_account.to_account_info(),
+            to: self.vault_token_account.to_account_info(),
+            authority: self.signer.to_account_info(),
+        };
+        let cpi_program = self.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::transfer(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+    fn mint_vtokens_to_user(&self, program_id: &Pubkey, amount: u64) -> Result<()> {
+        let (_, bump) = Pubkey::find_program_address(&[b"mint_authority"], program_id);
         let seeds: &[&[u8]; 2] = &[b"mint_authority", &[bump][..]];
         let signer = &[&seeds[..]];
         token::mint_to(
@@ -216,13 +207,14 @@ impl Deposit<'_> {
 
 #[derive(Accounts)]
 pub struct Withdraw<'info> {
-    #[account(signer)]
     pub signer: Signer<'info>,
     #[account(
         constraint = withdrawal_token_account.owner == signer.key(),
     )]
     #[account(mut)]
     pub withdrawal_token_account: Account<'info, TokenAccount>,
+
+    pub mint: Account<'info, Mint>,
 
     #[account(mut)]
     pub vault: Account<'info, Vault>,
@@ -240,6 +232,38 @@ impl Withdraw<'_> {
         );
         require!(!self.vault.is_frozen, MurkError::VaultFrozenError);
         require!(!self.vault.is_closed, MurkError::VaultClosedError);
+
+        Ok(())
+    }
+    fn withdraw_from_vault(&self, program_id: &Pubkey, amount: u64) -> Result<()> {
+        let vault_id_bytes: [u8; 8] = self.vault.id.to_le_bytes();
+        let (_, bump) = Pubkey::find_program_address(
+            &[b"vault", vault_id_bytes.as_ref()],
+            program_id,
+        );
+        let seeds: &[&[u8]; 3] = &[b"vault", vault_id_bytes.as_ref(), &[bump][..]];
+        let signer = &[&seeds[..]];
+
+        let cpi_program = self.token_program.to_account_info();
+        let cpi_accounts = Transfer {
+            from: self.vault_token_account.to_account_info(),
+            to: self.withdrawal_token_account.to_account_info(),
+            authority: self.vault.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
+        token::transfer(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+    fn burn_vtoken_from_user(&self, amount: u64) -> Result<()> {
+        let cpi_accounts = Burn {
+            mint: self.mint.to_account_info(),
+            from: self.vault_token_account.to_account_info(),
+            authority: self.signer.to_account_info(),
+        };
+        let cpi_program = self.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::burn(cpi_ctx, amount)?;
 
         Ok(())
     }
